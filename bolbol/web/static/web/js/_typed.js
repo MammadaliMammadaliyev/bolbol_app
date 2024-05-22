7195b34ccbd8a73c62b4ab36ec5351f91784fe9d
MammadaliMammadaliyev <mammadaliyevmammadali@gmail.com>
2024-05-22 11:29:37 +0400

    static files: extended _types.js, transport sub-filters fix

--- bolbol/web/static/web/js/_typed.js
+++ bolbol/web/static/web/js/_typed.js
@@ -13,7 +13,14 @@ if ($('#home-typed').length > 0) {
 if ($('#transport-typed').length > 0) {
     console.log("transport search")
     var typed = new Typed('#transport-typed', {
-        strings: ["Toyota Land Cruiser", 'iPhone 12', 'Pərdə', 'Maşın təkəri', 'Xiaomi Mi9T', 'Rəqs dərsləri', 'və sairə...'],
+        strings: [
+            "Toyota Land Cruiser",
+            "BMW 328i, 2018 il",
+            "Toyota Camry, 2020 il",
+            "Hyundai Elantra, 2019 il",
+            "Ford Mustang, 2021 il",
+            "Mercedes-Benz C200, 2017 il"
+        ],
         typeSpeed: 40,
         backSpeed: 20,
         attr: 'placeholder',
@@ -24,7 +31,13 @@ if ($('#transport-typed').length > 0) {
 
 if ($('#estate-typed').length > 0) {
     var typed = new Typed('#estate-typed', {
-        strings: ["Nerimanov 2 otaqli", 'iPhone 12', 'Pərdə', 'Maşın təkəri', 'Xiaomi Mi9T', 'Rəqs dərsləri', 'və sairə...'],
+        strings: [
+            "Nərimanov metrosu, 2 otaqlı",
+            "28 May metrosu, 3 otaqlı",
+            "Elmlər Akademiyası metrosu, 1 otaqlı",
+            "Xətai metrosu, 4 otaqlı",
+            "Gənclik metrosu, 2 otaqlı"
+        ],
         typeSpeed: 40,
         backSpeed: 20,
         attr: 'placeholder',
